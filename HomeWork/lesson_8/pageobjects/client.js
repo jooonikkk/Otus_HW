b07de34c4b0514768367f31073daecde7e474ea0
jooonikkk <yushin.alexey2014@yandex.ru>
2024-04-15 23:40:30 +0300

    hw8

--- HomeWork/lesson_8/pageobjects/client.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-import config from '../../tests.config'
-
-const client = axios.create({
-  baseURL: config.url,
-  validateStatus: () => true,
-})
-
-export default client
--- HomeWork/tests.config.js
+++
@@ -1,9 +0,0 @@
-import { faker } from '@faker-js/faker'
-
-const config = {
-  // Base url
-  url: "https://bookstore.demoqa.com",
-  password: "Otus_QA_JS_Vansa/24@#!^"
-}
-
-export default config;
